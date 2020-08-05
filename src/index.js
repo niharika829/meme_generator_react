6bfda2be6856befb402ac6dd6afc8d8c8e731987
niharika829 <ns1324y@gmail.com>
2020-08-06 03:59:27 +0530

    first commit

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "bootstrap/dist/css/bootstrap.css";
+import Meme from "./components/meme/main.js";
+
+ReactDOM.render(<Meme />, document.getElementById("root"));
+
